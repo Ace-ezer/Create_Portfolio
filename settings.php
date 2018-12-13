385718d9b1e8d2a62253d2e93a9877f659dbaa01
Arpit Yadav <34399051+Ace-ezer@users.noreply.github.com>
2018-12-13 19:44:08 +0530

    Add files via upload

---
+++ settings.php
@@ -0,0 +1,12 @@
+<?php
+
+/* Google App Client Id */
+define('CLIENT_ID', 'your_client_id');
+
+/* Google App Client Secret */
+define('CLIENT_SECRET', 'your_client_secret');
+
+/* Google App Redirect Url */
+define('CLIENT_REDIRECT_URL', 'http://localhost/portfolio/googlelogin.php');
+
+?>
